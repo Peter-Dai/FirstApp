703edb0afb35b619a1478b4343abc1cd42e9157d
Peter Dai <peter.dai@perficient.com>
2018-04-02 21:46:33 +0800

    Check in

---
+++ src/components/ReactValidation/htmlField/ReactText.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { withFormApiContext } from "../context/withFormApiContext";
+
+class ReactText extends Component {
+    constructor(...props) {
+        super(...props)
+    }
+
+    componentDidMount() {
+        //init form state for validation.
+        let { name, formApi } = this.props;
+        formApi.initFormStateValue(name);
+    }
+
+    render() {
+        // self props(name,type,onChange) and context props(formApi)
+        const { name, type, onChange, formApi } = this.props;
+        return (
+            <input type='text'
+                name={name}
+                onChange={(e) => {
+                    formApi.handleChange(e);
+                    if (onChange) {
+                        onChange(e)
+                    }
+                }}
+                value={formApi.getFormStateValue} />
+        );
+    }
+}
+
+export default withFormApiContext(ReactText);
